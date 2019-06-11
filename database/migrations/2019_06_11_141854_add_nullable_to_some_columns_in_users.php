0faaf46530e0649700fbc77c570460a37d207a44
Andi Rayka <andi.rayka@gmail.com>
2019-06-11 21:37:38 +0700

    Install dbal dan tambah nullable di beberapa kolom users

---
+++ database/migrations/2019_06_11_141854_add_nullable_to_some_columns_in_users.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNullableToSomeColumnsInUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('no_telpon')->nullable()->change();
+            $table->string('alamat')->nullable()->change();
+            $table->string('jml_transaksi')->nullable()->change();
+            $table->string('total_donasi')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('no_telpon')->nullable($value = false)->change();
+            $table->string('alamat')->nullable($value = false)->change();
+            $table->string('jml_transaksi')->nullable($value = false)->change();
+            $table->string('total_donasi')->nullable($value = false)->change();
+        });
+    }
+}
