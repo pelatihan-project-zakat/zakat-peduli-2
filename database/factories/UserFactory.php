9a032235d6f5f9e9e80263f94e7ba5aed1c068a4
Andi Rayka <andi.rayka@gmail.com>
2019-06-12 21:54:13 +0700

    Tampilan admin/user selesai

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -8,10 +8,16 @@ use Faker\Generator as Faker;
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'status' => 'bentar',
         'email' => $faker->unique()->safeEmail,
+    
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+
+        'no_telpon' => $faker->phoneNumber,
+        'alamat' => $faker->address,
+        'jml_transaksi' => $faker->randomDigit,
+        'total_donasi' => $faker->numberBetween($min = 100000, $max = 90000000)
+
     ];
 });
