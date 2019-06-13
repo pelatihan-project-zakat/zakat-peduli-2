6b15be89cafcdb0e494340f4e54ee8ca527ef768
BellaMardiana <bella_mardiana_25rpl@student.smktelkom-mlg.sch.id>
2019-06-14 01:29:37 +0700

    admin page required

--- resources/views/admin/bank/create.blade.php
+++ resources/views/admin/bank/create.blade.php
@@ -10,16 +10,16 @@
             <form action="{{route('bank.store')}}" method="POST" enctype="multipart/form-data">
                 @csrf
                 <label for="name">Nama Bank</label>
-                <input type="text" class="form-control" placeholder="Nama Bank" name="nama_bank">
+                <input type="text" class="form-control" placeholder="Nama Bank" name="nama_bank" required="required">
                 <br>
 
                 <label for="address">No. Rekening Donasi</label>
-                <input type="text" class="form-control" placeholder="No. Rek" name="no_rek">
+                <input type="text" class="form-control" placeholder="No. Rek" name="no_rek" required="required">
                 <br>
 
                 <div class="form-group">
                     <label for="image" class="label">Logo Bank</label> <br>
-                    <input type="file" class="form-controll" name="image">
+                    <input type="file" class="form-controll" name="image" required="required">
                     <br>
                     {{-- Tampilan masih hambret --}}
                 </div>
--- resources/views/admin/mustahiq/edit.blade.php
+++ resources/views/admin/mustahiq/edit.blade.php
@@ -11,11 +11,11 @@
                 @method('PUT')
                 @csrf
                 <label for="name">Nama Mustahiq</label>
-                <input type="text" class="form-control" placeholder="Nama Mustahiq" name="nama_mustahiq" value="{{$mustahiq->nama_mustahiq}}">
+                <input type="text" class="form-control" placeholder="Nama Mustahiq" name="nama_mustahiq" value="{{$mustahiq->nama_mustahiq}}" required="required">
                 <br>                
 
                 <label for="address">Alamat Instansi</label>
-                <textarea class="form-control" placeholder="Alamat Instansi" rows="4" name="alamat_mustahiq">{{$mustahiq->alamat_mustahiq}}</textarea>
+                <textarea class="form-control" placeholder="Alamat Instansi" rows="4" name="alamat_mustahiq" required="required">{{$mustahiq->alamat_mustahiq}}</textarea>
                 <br>
 
                 <button type="submit" class="btn btn-info">Edit Data</button>
